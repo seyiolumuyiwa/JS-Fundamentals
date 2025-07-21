7982a1438988bbf33af5b44018318491477944d3
seyi olumuyiwa <dagaria964@gmail.com>
2025-07-21 12:27:21 +0100

    Fixed test 5

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,7 @@
 const [firstArg] = process.argv.slice(2);
 
 if (!isNaN(firstArg)) {
-  console.log ("My number: ", firstArg);
+  console.log ("My number:", firstArg);
 } else {
   console.log ('Not a number');
 }

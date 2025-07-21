a7828f890f01126b94f9b9a7ad48d8ddab3ff753
seyi olumuyiwa <dagaria964@gmail.com>
2025-07-21 18:58:40 +0100

    Fixed tast 10

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,8 +1,12 @@
 const [firstArg] = process.argv.slice(2);
-let total = []
-      for (let i = 1; i <= firstArg; i++) {
+    let total = []
+      function fact() {
+  
+        for (let i = 1; i <= firstArg; i++) {
     total.push(i)
       }
       let result = total.reduce((acc, val) => acc * val, 1)
-    
-      console.log(result);
+      console.log(result)
+      }
+fact()
+      

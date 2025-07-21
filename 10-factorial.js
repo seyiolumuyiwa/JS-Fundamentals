7c2bf0408075c8eeda3679b2e9f156f97a39aad9
seyi olumuyiwa <dagaria964@gmail.com>
2025-07-21 18:45:53 +0100

    Task 10

---
+++ 10-factorial.js
@@ -0,0 +1,8 @@
+const [firstArg] = process.argv.slice(2);
+let total = []
+      for (let i = 1; i <= firstArg; i++) {
+    total.push(i)
+      }
+      let result = total.reduce((acc, val) => acc * val, 1)
+    
+      console.log(result);

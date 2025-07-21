3a09568a756a6b80d792c8b5180133dda2767aaa
seyi olumuyiwa <dagaria964@gmail.com>
2025-07-21 17:08:42 +0100

    Task 8

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const [firstArg] = process.argv.slice(2);
+let square= 'x';
+ if (isNaN(firstArg)) {
+      console.log ('Missing size')
+    } else {
+       for (let i = 0; i < +firstArg; i++) {
+    console.log(square.repeat(firstArg));
+  }
+    }
